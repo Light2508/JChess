60aef0954baca6d658e04a6e4ab5b522fd6da563
Light2508 <davidd1900a@gmail.com>
2026-01-18 13:46:50 +0100

    add MVC basic files

---
+++ src/main/java/Controller.java
@@ -0,0 +1,9 @@
+public class Controller {
+
+    // Menu
+
+    void startGame() {
+
+    }
+
+}
---
+++ src/main/java/Piece.java
@@ -0,0 +1,7 @@
+public abstract class Piece {
+
+    protected int posx;
+    protected int
+
+    public void move(){};
+}
